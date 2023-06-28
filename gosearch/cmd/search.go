c9662d54a6497d72f1cc44fea22c4bc86ca3ec2e
Georgiy Bykov <zhora.bykov@gmail.com>
2023-06-29 02:12:23 +0300

    Methods refactoring

--- gosearch/cmd/search.go
+++ gosearch/cmd/search.go
@@ -12,81 +12,53 @@ import (
 var (
 	urls  = [2]string{"https://go.dev", "https://golang.org"}
 	depth = 1 // 3
-	word  string
 )
 
-// flag.StringVar(&word, "s", "", "search query")
-// flag.Parse()
-
-// fmt.Println(word)
-// fmt.Println(os.Args[1:])
-
 func main() {
-	// готовит параметры +
-
-	// запускает сканирование по сайтам +
-	// объединяет результаты в нумерованный список +
-
-	// если есть параметр поиска фильтрует результаты и возвращает мутированный список +
-
 	fmt.Println("Start searching...")
 
-	parseArgs()
-
-	documents := scan(urls, depth)
-
-	printList(documents)
+	argument := extractArgument()
+	documents := scan(urls, depth, argument)
+	render(documents)
 }
 
-func parseArgs() {
-	flag.StringVar(&word, "s", "", "Parameter for search")
-	flag.Parse()
-
-	if len(word) > 0 {
-		word = strings.ToLower(word)
-	}
-}
-
-func scan(urls [2]string, depth int) []crawler.Document {
-	var documents []crawler.Document
-
+func scan(urls [2]string, depth int, argument string) (documents []crawler.Document) {
 	scanner := spider.New()
 
 	for _, url := range urls {
 		result, error := scanner.Scan(url, depth)
 		if error != nil {
-			fmt.Printf("Error occurs while searching %q\n", url)
+			fmt.Printf("An error occurred while searching by URL %q\n", url)
 			continue
 		}
 
 		documents = append(documents, result...)
 	}
 
-	if len(word) > 0 {
-		return filter(documents)
+	if len(argument) > 0 {
+		return filter(documents, argument)
 	}
-
 	return documents
 }
 
-func filter(documents []crawler.Document) []crawler.Document {
-	var filteredDocuments []crawler.Document
-
+func filter(documents []crawler.Document, argument string) (filteredDocuments []crawler.Document) {
 	for _, document := range documents {
 		title := strings.ToLower(document.Title)
 
-		if strings.Contains(title, word) {
+		if strings.Contains(title, argument) {
 			filteredDocuments = append(filteredDocuments, document)
 		}
 	}
 	return filteredDocuments
 }
 
-func printList(documents []crawler.Document) {
+func render(documents []crawler.Document) {
 	if len(documents) == 0 {
 		fmt.Println("Documents not found")
 		return
 	}
+
+	fmt.Println("Documets:")
 
 	for index, document := range documents {
 		if document.Body != "" {
@@ -95,4 +67,14 @@ func printList(documents []crawler.Document) {
 			fmt.Printf("%d. %q (%q)\n", index, document.Title, document.URL)
 		}
 	}
+}
+
+func extractArgument() (argument string) {
+	flag.StringVar(&argument, "s", "", "Parameter for search")
+	flag.Parse()
+
+	if len(argument) > 0 {
+		argument = strings.ToLower(argument)
+	}
+	return argument
 }
