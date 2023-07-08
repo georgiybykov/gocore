5283ec17ac2771b01078e9a6c02c18a840c7b025
Georgiy Bykov <zhora.bykov@gmail.com>
2023-07-09 01:58:29 +0300

    Copy homework 3 core to homework 5

---
+++ homework-05/pkg/crawler/membot/membot.go
@@ -0,0 +1,32 @@
+package membot
+
+import (
+	"gocore/homework-05/pkg/crawler"
+)
+
+// Service - имитация службы поискового робота.
+type Service struct{}
+
+// New - конструктор имитации службы поискового робота.
+func New() *Service {
+	s := Service{}
+	return &s
+}
+
+// Scan возвращает заранее подготовленный набор данных.
+func (s *Service) Scan(url string, depth int) ([]crawler.Document, error) {
+	data := []crawler.Document{
+		{
+			ID:    0,
+			URL:   "https://yandex.ru",
+			Title: "Яндекс",
+		},
+		{
+			ID:    1,
+			URL:   "https://google.ru",
+			Title: "Google",
+		},
+	}
+
+	return data, nil
+}
---
+++ homework-05/pkg/index/index_test.go
@@ -0,0 +1,86 @@
+package index
+
+import (
+	"reflect"
+	"testing"
+
+	"gocore/homework-05/pkg/crawler"
+)
+
+func TestAppend(t *testing.T) {
+	documents := []crawler.Document{
+		{
+			ID:    1,
+			Title: "Two words",
+		},
+		{
+			ID:    3,
+			Title: "Three words here",
+		},
+	}
+
+	storage := New()
+	storage.Append(documents)
+
+	got := len(storage.Data)
+	want := 4
+	if got != want {
+		t.Errorf("expected '%d' but got '%d'", want, got)
+	}
+}
+
+func TestSearch(t *testing.T) {
+	documents := []crawler.Document{
+		{
+			ID:    1,
+			Title: "Two words",
+		},
+		{
+			ID:    5,
+			Title: "Three words here",
+		},
+		{
+			ID:    15,
+			Title: "Four words right here",
+		},
+	}
+
+	storage := New()
+	storage.Append(documents)
+
+	presets := []struct {
+		context string
+		lexeme  string
+		want    []int
+	}{
+		{
+			context: "When the expected lexeme is present in documents",
+			lexeme:  "WorDs",
+			want:    []int{1, 5, 15},
+		},
+		{
+			context: "When the expected lexeme is present in document",
+			lexeme:  "two",
+			want:    []int{1},
+		},
+		{
+			context: "When the expected lexeme is not found",
+			lexeme:  "Undefined",
+			want:    nil,
+		},
+		{
+			context: "When the expected lexeme is more than one word",
+			lexeme:  "words right",
+			want:    nil,
+		},
+	}
+
+	for _, preset := range presets {
+		t.Run(preset.context, func(t *testing.T) {
+			got := storage.Search(preset.lexeme)
+			if !reflect.DeepEqual(got, preset.want) {
+				t.Errorf("expected '%d' but got '%d'", preset.want, got)
+			}
+		})
+	}
+}
