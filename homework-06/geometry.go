84d2b6c248b63fa15fd9ec76fadf9d2f04c43e05
Georgiy Bykov <zhora.bykov@gmail.com>
2023-07-22 18:51:41 +0300

    Devide x && y arguments into x1, y1, x2, y2

--- homework-06/geometry.go
+++ homework-06/geometry.go
@@ -4,8 +4,8 @@ import (
 	"math"
 )
 
-func Distance(x, y float64) float64 {
-	return math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2))
+func Distance(x1, y1, x2, y2 float64) float64 {
+	return math.Sqrt(math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2))
 }
 
 /*
