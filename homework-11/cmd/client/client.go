ba957e7c08404036fde69e0d100fe3d1f5191b77
Georgiy Bykov <zhora.bykov@gmail.com>
2023-09-10 23:12:02 +0300

    Add netsrv package; cmd

--- homework-11/cmd/client/client.go
+++ homework-11/cmd/client/client.go
@@ -18,7 +18,6 @@ func main() {
 	defer fmt.Println("Connection closed")
 
 	terminal := bufio.NewReader(os.Stdin)
-	// server := bufio.NewReader(client.Conn)
 
 	for {
 		fmt.Println("Enter a lexeme to search for:")
@@ -31,22 +30,5 @@ func main() {
 		fmt.Printf("Searching for: %q\n", string(lexeme))
 
 		client.Search(string(lexeme))
-
-		// 	fmt.Fprintf(conn, "%s\n", string(lexeme))
-
-		// HANDLER:
-		// 	for {
-		// 		bytes, _, err := server.ReadLine()
-		// 		if err != nil {
-		// 			log.Fatal(err)
-		// 		}
-
-		// 		response := strings.TrimSpace(string(bytes))
-		// 		if len(response) == 0 {
-		// 			break HANDLER
-		// 		}
-
-		// 		fmt.Println(response)
-		// 	}
 	}
 }
